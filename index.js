98de904ff387c65e6de41c1fe5770b140cbb6a75
Mikaelen Oliveira <mikaelennilo@gmail.com>
2022-08-24 13:55:51 -0300

    instalando cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/consultas", (req, res) => {
   if (req.query.tipo == "contatos") {
